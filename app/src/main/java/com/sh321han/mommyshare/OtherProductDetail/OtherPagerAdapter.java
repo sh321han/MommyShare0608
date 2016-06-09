44de54a83dd67f03f23ff00f0df6feb1d3c8bcf7
sh321han <sh321han@naver.com>
2016-06-09 21:44:28 +0900

    0609

--- app/src/main/java/com/sh321han/mommyshare/OtherProductDetail/OtherPagerAdapter.java
+++ app/src/main/java/com/sh321han/mommyshare/OtherProductDetail/OtherPagerAdapter.java
@@ -2,14 +2,23 @@ package com.sh321han.mommyshare.OtherProductDetail;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.sh321han.mommyshare.R;
+import com.sh321han.mommyshare.data.ProductDetailData;
 
 /**
  * Created by Administrator on 2016-05-24.
  */
 public class OtherPagerAdapter extends PagerAdapter {
 
+    private static final String MOMMYSHARE_SERVER = "http://52.79.57.157:3000";
+    LayoutInflater inflater;
+    ProductDetailData data;
     Context mContext;
     public static final int VIEW_COUNT = 3;
 
@@ -28,7 +37,10 @@ public class OtherPagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
 
         View view = null;
-
+        view= inflater.inflate(R.layout.viewpager_childview, null);
+        ImageView img = (ImageView)view.findViewById(R.id.image_profile);
+        Glide.with(view.getContext()).load(MOMMYSHARE_SERVER + data.getPicture_path() + data.getPicture_name().get(0)).into(img);
+        container.addView(view);
 
         return view;
 
@@ -43,6 +55,8 @@ public class OtherPagerAdapter extends PagerAdapter {
     public boolean isViewFromObject(View view, Object object) {
         return view == object;
     }
+
+
 
 
 }
