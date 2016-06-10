9e982f48138144c3cc3e99f7728464b31eec30ea
sh321han <sh321han@naver.com>
2016-06-10 16:53:56 +0900

    0610

---
+++ app/src/main/java/com/sh321han/mommyshare/MyResultError.java
@@ -0,0 +1,9 @@
+package com.sh321han.mommyshare;
+
+/**
+ * Created by Tacademy on 2016-06-10.
+ */
+public class MyResultError {
+    public int code;
+    public String result;
+}
---
+++ app/src/main/java/com/sh321han/mommyshare/MyResultStatus.java
@@ -0,0 +1,8 @@
+package com.sh321han.mommyshare;
+
+/**
+ * Created by Tacademy on 2016-06-10.
+ */
+public class MyResultStatus {
+    public int code;
+}
